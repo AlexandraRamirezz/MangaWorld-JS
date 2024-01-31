0faf281ebbebcefd5c1d17ec6af1283de66454ca
Alexandra <alexa.dayanna07@gmail.com>
2024-01-30 21:33:06 -0500

    feat: created the comics array

---
+++ js/data.js
@@ -0,0 +1,10 @@
+const comics = [
+    { id: 1, name: 'BLUE LOCK, N.01', category: 'Sports', price: 60.00, stock: 10, img: 'img/card1.jpg', popular: true },
+    { id: 2, name: 'GIVEN, VOL. 2', category: 'Romance', price: 41.30, stock: 6, img: 'img/card2.png', popular: false },
+    { id: 3, name: 'GUARDIANES DE LA NOCHE, VOL. 12', category: 'Action', price: 55.70, stock: 15, img: 'img/card3.jpg', popular: true },
+    { id: 4, name: 'SPY X FAMILY 01', category: 'Comedy', price: 62.00, stock: 9, img: 'img/card4.jpg', popular: false },
+    { id: 5, name: 'CHAINSAW MAN 03', category: 'Horror', price: 49.90, stock: 12, img: 'img/card5.jpeg', popular: false },
+    { id: 6, name: 'EN LA MISMA CLASE 02', category: 'Escolar', price: 28.50, stock: 3, img: 'img/card6.jpg', popular: false },
+    { id: 7, name: 'ORANGE, VOL. 1', category: 'Romance', price: 39.99, stock: 4, img: 'img/card7.jpg', popular: false },
+    { id: 8, name: 'OSHI NO KO 01', category: 'Drama', price: 52.00, stock: 15, img: 'img/card8.jpg', popular: true },
+]
